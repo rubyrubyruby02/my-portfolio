ddf5b0159ca1f67447a3a792d27effa04f8cfc78
rubyrubyruby02 <ruby.porter@tiscali.co.uk>
2024-01-04 21:20:36 +0000

    replaces placeholder images with videos, changed icon and font size alignment across the page

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,28 @@
 import React from "react";
 
+
 export default function About(){
 
     return (
        <section id="about">
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Hi, I'm Ruby</h1>
-                <p className="mb-8 leading-relaxed">
+                <h1 className="title-font sm:text-4xl text-4xl mb-4 font-medium text-white">Hi, I'm Ruby</h1>
+                <h2 className="mb-8 leading-relaxed text-3xl">
                     I'm an IT manager, coder and travel-lover. I like blue cheese, brownies, last-minute weekend trip and a bit of running.
-                </p>
+                </h2>
             
             <div className="flex justify-center">
-                <a href="#projects"
-                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">See my work</a>
+                <a href="https://www.linkedin.com/in/ruby-riley02/"
+                className="inline-flex text-gray-400 bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg w-32 mr-4">
+                    <img src="../../assets/linkedin.png" alt="linkedIn"/>
+                </a>
+                <a href="https://www.linkedin.com/in/ruby-riley02/"
+                className="inline-flex text-gray-400 bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg w-32">
+                    <img src="../../assets/githubLogo.png" alt="github"/>
+                </a>
             </div>
+
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                 <img
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,17 @@
 import React from "react";
-import {CodeIcon} from "@heroicons/react/solid";
+//import {CodeIcon} from "@heroicons/react/solid";
 import { projects } from "../data";
+
+
+const buildingIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="mx-auto inline-block w-10 mb-4">
+        <path fillRule="evenodd" d="M3 2.25a.75.75 0 0 0 0 1.5v16.5h-.75a.75.75 0 0 0 0 1.5H15v-18a.75.75 0 0 0 0-1.5H3ZM6.75 19.5v-2.25a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75ZM6 6.75A.75.75 0 0 1 6.75 6h.75a.75.75 0 0 1 0 1.5h-.75A.75.75 0 0 1 6 6.75ZM6.75 9a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75ZM6 12.75a.75.75 0 0 1 .75-.75h.75a.75.75 0 0 1 0 1.5h-.75a.75.75 0 0 1-.75-.75ZM10.5 6a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75Zm-.75 3.75A.75.75 0 0 1 10.5 9h.75a.75.75 0 0 1 0 1.5h-.75a.75.75 0 0 1-.75-.75ZM10.5 12a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75ZM16.5 6.75v15h5.25a.75.75 0 0 0 0-1.5H21v-12a.75.75 0 0 0 0-1.5h-4.5Zm1.5 4.5a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Zm.75 2.25a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75v-.008a.75.75 0 0 0-.75-.75h-.008ZM18 17.25a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Z" clipRule="evenodd" />
+        </svg>
+    )
+}
+
+
 
 export default function Projects(){
    
@@ -8,9 +19,9 @@ export default function Projects(){
         <section id="projects" className="text-gray-400 bg-gray-900 body-font">
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
                 <div className=" flex flex-col w-full mb-20">
-                    <CodeIcon className="mx-auto inline-block w-10 mb-4"/>
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-text-white">Apps I've built</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                    {buildingIcon()}
+                    <h2 className="sm:text-4xl text-3xl font-medium title-font mb-text-white">Apps I've built</h2>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-xl mt-4">
                         Hover over each tile to find out more or click to view individual projects.
                     </p>
                 </div>
@@ -21,11 +32,14 @@ export default function Projects(){
                         key = {project.image}
                         className="sm:w-1/2 w-100 p-4">
                             <div className="flex relative">
-                                <img 
-                                alt="gallery of images"
-                                className="absolute inset-0 w-full h-full object-cover object-center"
-                                src={project.image}
-                                />
+                                <video playsInline autoPlay muted loop
+                                alt="video of northcoder's new website"
+                                className="absolute inset-0 w-full h-full object-center"
+                                >
+                                    <source 
+                                    src={project.image} type="video/mp4"></source>
+                                </video>
+
                             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                                 <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                                     {project.subtitle}
